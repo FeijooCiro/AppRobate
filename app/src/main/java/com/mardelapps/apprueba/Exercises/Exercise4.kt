e46b549deac3bec1b1c83400b949b57b0810f21d
Ciro Feijoo <cfeijoo@infosis.tech>
2023-10-02 09:39:47 -0300

    Comienzo del ejercicio 4 de android developer. Sin terminar resolver.

---
+++ app/src/main/java/com/mardelapps/apprueba/Exercises/Exercise4.kt
@@ -0,0 +1,28 @@
+package com.mardelapps.apprueba.Exercises
+
+import com.mardelapps.apprueba.Exercises.Song as Song
+
+class Song{
+    private var titulo:String = "man"
+    private var nombreArtista:String = "Roberto"
+    private var anioPublicacion:Number = 1945
+    private var reproducciones:Number = 304
+
+    fun popularidad(reproduc:Number): String {
+        reproduc = this.reproducciones
+        if(reproduc < 1000){
+            return " poco popular."
+        } else {
+            return " popular."
+        }
+    }
+
+    fun mostrarDatos(){
+        println("$titulo, interpretada por $nombreArtista, lanzada en $anioPublicacion. Fue ${popularidad()}")
+    }
+}
+
+fun main(){
+    val cancion = Song()
+    cancion.mostrarDatos()
+}
