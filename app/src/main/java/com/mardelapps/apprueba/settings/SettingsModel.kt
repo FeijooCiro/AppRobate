42162c55cf98aaad6140bf54e35cb180ced4410b
Ciro Feijoo <cfeijoo@infosis.tech>
2023-10-20 11:33:33 -0300

    Proyecto Aprueba terminado.

---
+++ app/src/main/java/com/mardelapps/apprueba/settings/SettingsModel.kt
@@ -0,0 +1,10 @@
+package com.mardelapps.apprueba.settings
+
+data class SettingsModel(
+    var volume: Int,
+    var bluetooth: Boolean,
+    var vibration: Boolean,
+    var darkMode: Boolean
+) {
+
+}
